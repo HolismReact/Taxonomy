cf77299ead73b897cb2db375abe0ed7a4787478f
Saeed Nemati <sn.1361@gmail.com>
2022-01-29 13:33:43 +0330

    committed by script

--- Admin/Tag/List.js
+++ Admin/Tag/List.js
@@ -1,5 +1,28 @@
+import { List, Text } from '@List'
+
+const filters = <>
+    <Text
+        column='Name'
+        placeholder='Name'
+    />
+</>
+
+const headers = <>
+    <th>Name</th>
+</>
+
+const row = (item) => <>
+    <td>{item.name}</td>
+</>
+
 const Tags = () => {
-    return <div>Tags</div>
+    return <List
+        title='Tags'
+        entityType='Tag'
+        filters={filters}
+        headers={headers}
+        row={row}
+    />
 }
 
 export default Tags
